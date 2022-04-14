b146468761f82d26024c0b0483f8401bac1a0cc9
Strashko Maksim <maksim000072@gmail.com>
2022-04-14 22:07:41 +1000

    Files upload

--- src/form-data/my-form-data-config.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MyFormDataConfigService } from './my-form-data-config.service';
-
-@Module({
-  providers: [MyFormDataConfigService],
-  exports: [MyFormDataConfigService],
-})
-export class MyFormDataConfigModule {}
