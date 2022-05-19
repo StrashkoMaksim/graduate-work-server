d2f025bcc68739bd2ea0fab5022663b6d9ac126f
Strashko Maksim <maksim000072@gmail.com>
2022-05-19 11:30:40 +1000

    Orders carts API save

--- src/modules/orders-carts/orders-carts.controller.ts
+++ src/modules/orders-carts/orders-carts.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Put } from '@nestjs/common';
+import { OrdersCartsService } from './orders-carts.service';
+import { UpdateOrderCartDto } from './dto/update-order-cart-dto';
+import { IdDto } from '../../validation/id-dto';
 
 @Controller('orders-carts')
-export class OrdersCartsController {}
+export class OrdersCartsController {
+  constructor(private ordersCartsService: OrdersCartsService) {}
+
+  @Put(':id')
+  updateOrderCart(@Body() dto: UpdateOrderCartDto, @Param() orderId: IdDto) {
+    return this.ordersCartsService.updateOrderCart(dto, orderId.id);
+  }
+}
--- src/modules/orders-carts/orders-carts.module.ts
+++ src/modules/orders-carts/orders-carts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersCartsController } from './orders-carts.controller';
 import { OrdersCartsService } from './orders-carts.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { OrdersCart } from './orders-carts.model';
+import { OrdersModule } from '../orders/orders.module';
 
 @Module({
   controllers: [OrdersCartsController],
-  providers: [OrdersCartsService]
+  providers: [OrdersCartsService],
+  imports: [SequelizeModule.forFeature([OrdersCart]), OrdersModule],
 })
 export class OrdersCartsModule {}
